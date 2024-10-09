8f04fa382cd571ef15a4332fa515dff9b1a16aed
KLN971 <Klautric@gmail.com>
2024-10-09 19:19:25 -0400

    ajout metrics, alertes et script de lancement

--- serviceClient/index.js
+++ serviceClient/index.js
@@ -4,7 +4,9 @@ const express = require('express');
 const request = require('request');
 const { loggerProvider } = require('./monitoring');
 const opentelemetry = require('@opentelemetry/api');
+
 const logsAPI = require('@opentelemetry/api-logs');
+const logger = logsAPI.logs.getLogger('default');
 
 const logger = loggerProvider.getLogger('serviceClient');
 
